75600fb69ece2ea2574f0c7529bf365fc33a0057
veronicapava <veronicapava@gmail.com>
2022-02-19 16:19:45 -0500

    Mejora del metodo get y creacion del metodo post. Los dos funcionando correctamente

--- src/main/java/com/example/tallerdosspring/controller/ProjectContoller.java
+++ src/main/java/com/example/tallerdosspring/controller/ProjectContoller.java
@@ -1,15 +1,11 @@
 package com.example.tallerdosspring.controller;
 
-
-import com.example.tallerdosspring.model.Employee;
 import com.example.tallerdosspring.model.Project;
 import com.example.tallerdosspring.repository.IProjectJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,15 +16,29 @@ public class ProjectContoller {
     @Autowired
     IProjectJpaRepository projectRepository;
 
+    //Metodo para obtener todos los proyectos
     @GetMapping()
     public ResponseEntity<List<Project>> getProjects(){
         try {
-            List<Project> proj = new ArrayList<Project>();
-
-            return new ResponseEntity<>(proj, HttpStatus.OK);
+            List<Project> projects = new ArrayList<Project>();
+            projectRepository.findAll().forEach(projects::add);
+            if (projects.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(projects, HttpStatus.OK);
         } catch (Exception e){
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
+    //Metodo para agregar un proyecto
+    @PostMapping("/post")
+    public ResponseEntity<Project> addNewProject(@RequestBody Project project){
+        try{
+            Project projectito = projectRepository.save(new Project(project.getName()));
+            return new ResponseEntity<>(projectito, HttpStatus.CREATED);
+        } catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.EXPECTATION_FAILED);
+        }
+    }
 }
