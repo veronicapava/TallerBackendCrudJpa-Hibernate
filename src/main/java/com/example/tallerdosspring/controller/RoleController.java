351bd734a04819b12e2a7c5c8818171423a10970
veronicapava <veronicapava@gmail.com>
2022-02-19 13:00:59 -0500

    Crea controlador para Role

--- src/main/java/com/example/tallerdosspring/controller/RoleController.java
+++ src/main/java/com/example/tallerdosspring/controller/RoleController.java
@@ -1,2 +1,32 @@
-package com.example.tallerdosspring.controller;public class RoleController {
+package com.example.tallerdosspring.controller;
+
+import com.example.tallerdosspring.model.Employee;
+import com.example.tallerdosspring.repository.IRoleJpaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/role")
+public class RoleController {
+
+    @Autowired
+    IRoleJpaRepository roleRepository;
+
+    @GetMapping()
+    public ResponseEntity<List<Employee>> getRoles(){
+        try {
+            List<Employee> emplo = new ArrayList<Employee>();
+
+            return new ResponseEntity<>(emplo, HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
