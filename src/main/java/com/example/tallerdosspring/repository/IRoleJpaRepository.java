c8f9a8e95a2b3431c08e1a6a9eb629bbe7221e69
veronicapava <veronicapava@gmail.com>
2022-02-18 20:56:13 -0500

    Actualiza interface IRole

--- src/main/java/com/example/tallerdosspring/repository/IRoleJpaRepository.java
+++ src/main/java/com/example/tallerdosspring/repository/IRoleJpaRepository.java
@@ -1,5 +1,6 @@
 package com.example.tallerdosspring.repository;
 
+import com.example.tallerdosspring.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IRoleJpaRepository extends JpaRepository<Role,Long> {
