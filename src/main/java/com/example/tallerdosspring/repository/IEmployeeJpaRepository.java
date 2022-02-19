0d7239111b609d4bebbba3c4abcc69b3db9227f7
veronicapava <veronicapava@gmail.com>
2022-02-18 19:41:43 -0500

    Crea interfaz para buscar empleado por id

--- src/main/java/com/example/tallerdosspring/repository/IEmployeeJpaRepository.java
+++ src/main/java/com/example/tallerdosspring/repository/IEmployeeJpaRepository.java
@@ -1,2 +1,12 @@
-package com.example.tallerdosspring.repository;public interface IEmployeeJpaRepository {
+package com.example.tallerdosspring.repository;
+
+import com.example.tallerdosspring.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IEmployeeJpaRepository extends JpaRepository<Employee, Long> {
+    //Select fields from employee where employeeid='[param]'
+    Employee findByEmployeeId(String employeeid);
+
 }
