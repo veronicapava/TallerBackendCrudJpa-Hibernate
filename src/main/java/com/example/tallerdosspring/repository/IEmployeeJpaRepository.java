8916cdd35dfffc896aef8ad4e8c5b633bce700cf
veronicapava <veronicapava@gmail.com>
2022-02-18 21:21:41 -0500

    Añade findByRole

--- src/main/java/com/example/tallerdosspring/repository/IEmployeeJpaRepository.java
+++ src/main/java/com/example/tallerdosspring/repository/IEmployeeJpaRepository.java
@@ -1,6 +1,7 @@
 package com.example.tallerdosspring.repository;
 
 import com.example.tallerdosspring.model.Employee;
+import com.example.tallerdosspring.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +12,5 @@ public interface IEmployeeJpaRepository extends JpaRepository<Employee, Long> {
     //Select fields from employee where employeeid='[param]'
     Employee findByEmployeeId(String employeeid);
     List<Employee> findByLastName(String lastName);
+    List<Employee> findByRole(Role role);
 }
