195bad609caaa0d09eadd75311fae32d74dac4f6
veronicapava <veronicapava@gmail.com>
2022-02-18 20:46:03 -0500

    Crea interface Project

--- src/main/java/com/example/tallerdosspring/repository/IProjectJpaRepository.java
+++ src/main/java/com/example/tallerdosspring/repository/IProjectJpaRepository.java
@@ -1,2 +1,7 @@
-package com.example.tallerdosspring.repository;public interface IProjectJpaRepository {
+package com.example.tallerdosspring.repository;
+
+import com.example.tallerdosspring.model.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IProjectJpaRepository extends JpaRepository<Project, Long> {
 }
