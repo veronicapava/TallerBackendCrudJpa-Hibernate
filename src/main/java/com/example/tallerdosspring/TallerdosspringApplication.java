2d9e54e040fb96ed79ed63bfcc659fb8b6ae65a0
veronicapava <veronicapava@gmail.com>
2022-02-18 19:15:36 -0500

    Inicio del proyecto

---
+++ src/main/java/com/example/tallerdosspring/TallerdosspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.tallerdosspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerdosspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerdosspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tallerdosspring/TallerdosspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tallerdosspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerdosspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
