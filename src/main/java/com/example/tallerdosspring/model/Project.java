5da5f1e62d13598611fb7c9a0c64b2984a86e598
veronicapava <veronicapava@gmail.com>
2022-02-18 20:45:25 -0500

    Modifica IEmployee, añadiendo findByLastName

---
+++ src/main/java/com/example/tallerdosspring/model/Project.java
@@ -0,0 +1,2 @@
+package com.example.tallerdosspring.model;public class Project {
+}
--- src/main/java/com/example/tallerdosspring/repository/IEmployeeJpaRepository.java
+++ src/main/java/com/example/tallerdosspring/repository/IEmployeeJpaRepository.java
@@ -4,9 +4,11 @@ import com.example.tallerdosspring.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IEmployeeJpaRepository extends JpaRepository<Employee, Long> {
     //Select fields from employee where employeeid='[param]'
     Employee findByEmployeeId(String employeeid);
-
+    List<Employee> findByLastName(String lastName);
 }
