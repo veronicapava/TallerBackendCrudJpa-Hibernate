d769001f1374942be9e71277f25ead30de55196e
veronicapava <veronicapava@gmail.com>
2022-02-19 16:20:24 -0500

    Cambio menor

--- src/main/java/com/example/tallerdosspring/repository/IProjectJpaRepository.java
+++ src/main/java/com/example/tallerdosspring/repository/IProjectJpaRepository.java
@@ -1,5 +1,7 @@
 package com.example.tallerdosspring.repository;
 
+import java.util.List;
+
 import com.example.tallerdosspring.model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
